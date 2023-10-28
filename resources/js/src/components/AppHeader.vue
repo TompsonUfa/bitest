5db6944a007c838efb1eb96261aafde77bc75f74
Roman <romanrus98@mail.ru>
2023-10-28 10:44:13 +0500

    Правка стилей header

--- resources/js/src/components/AppHeader.vue
+++ resources/js/src/components/AppHeader.vue
@@ -2,9 +2,9 @@
     <header class="header">
         <div class="container">
             <nav class="navbar p-0 justify-content-between">
-                <router-link class="navbar-brand" to="/">BITEST</router-link>
+                <router-link class="navbar-brand" to="/">bitest</router-link>
                 <div class="d-flex align-items-center gap-2">
-                    <div @click="this.toggleModal('search')" class="btn navbar__search">
+                    <div @click="this.toggleModal('search')" class="header__search">
                         <i class="bi bi-search"></i>
                     </div>
                     <profile-nav></profile-nav>
@@ -31,5 +31,13 @@ export default {
 <style scoped lang="scss">
     .header {
         height: var(--header-height);
+        border-bottom: 1px solid #f5f5f5;
+        .navbar-brand{
+            text-transform: uppercase;
+            letter-spacing: 1.2px;
+        }
+        &__search {
+            cursor: pointer;
+        }
     }
 </style>
